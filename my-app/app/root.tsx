e75b39d57a7d15b1336b0fd7aff6e6a1a00b3b32
Alexcb7 <alex.cortell.ifp@gmail.com>
2025-10-24 11:53:04 +0200

    segunda subida javier

--- my-app/app/root.tsx
+++ my-app/app/root.tsx
@@ -31,7 +31,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        Hello
       </head>
       <body>
         {children}
